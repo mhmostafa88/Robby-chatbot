661697e0c477fc6882506b22e378cbc37dfcb9bd
mhmostafa88 <mahmoud@mightydigits.com>
2023-06-04 21:18:38 +0300

    api key saved in the env

--- src/modules/utils.py
+++ src/modules/utils.py
@@ -17,19 +17,20 @@ class Utilities:
         if not hasattr(st.session_state, "api_key"):
             st.session_state.api_key = None
         #you can define your API key in .env directly
-        if os.path.exists(".env") and os.environ.get("OPENAI_API_KEY") is not None:
-            user_api_key = os.environ["OPENAI_API_KEY"]
-            st.sidebar.success("API key loaded from .env", icon="🚀")
-        else:
-            if st.session_state.api_key is not None:
-                user_api_key = st.session_state.api_key
-                st.sidebar.success("API key loaded from previous input", icon="🚀")
-            else:
-                user_api_key = st.sidebar.text_input(
-                    label="#### Your OpenAI API key 👇", placeholder="sk-...", type="password"
-                )
-                if user_api_key:
-                    st.session_state.api_key = user_api_key
+        user_api_key = st.secrets["OPENAI_API_KEY"]
+        # if os.path.exists(".env") and os.environ.get("OPENAI_API_KEY") is not None:
+        #     user_api_key = os.environ["OPENAI_API_KEY"]
+        #     st.sidebar.success("API key loaded from .env", icon="🚀")
+        # else:
+        #     if st.session_state.api_key is not None:
+        #         user_api_key = st.session_state.api_key
+        #         st.sidebar.success("API key loaded from previous input", icon="🚀")
+        #     else:
+        #         user_api_key = st.sidebar.text_input(
+        #             label="#### Your OpenAI API key 👇", placeholder="sk-...", type="password"
+        #         )
+        #         if user_api_key:
+        #             st.session_state.api_key = user_api_key
 
         return user_api_key
 
